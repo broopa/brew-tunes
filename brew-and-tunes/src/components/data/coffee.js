722d9dce00910bac054f4673177e437b14725f44
Ian-CM <30392014+broopa@users.noreply.github.com>
2022-03-16 16:27:34 -0400

    New Updates & CSS
    Updated Selections & responsive styles for iframes.

--- brew-and-tunes/src/components/data/coffee.js
+++ brew-and-tunes/src/components/data/coffee.js
@@ -1,13 +1,13 @@
 export const WeeklyCoffee = {
-        name: 'Ethiopia Sidamo Guji',
-        roaster: 'Batdorf & Bronson',
+        name: 'Misty Valley',
+        roaster: 'Many Worlds Tavern',
         origin: 'Yirga Chefe, Ethiopia',
-        imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
-        desc: 'Natural processed and fruit-forward with tantalizing aromatics. We find flavors of ripe berries including blueberry and blackberry. The cup is balanced and heavy bodied with a unique spicy finish of both nutmeg and clove.',
+        imageUrl: 'https://cdn.shopify.com/s/files/1/0521/0921/3887/t/8/assets/treasured-realm--web-1637297829659.png?v=1637297838',
+        desc: 'Naturally-processed Ethiopian coffee is prized for its fruitiness and complexity.  The Misty Valley is a beautiful example from the Yirgacheffe region. With a coffee this fruity and intensely flavored, we take a lighter approach to the roasting profile to let the beans shine on their own. From producer Abdullah Bagersh, the ripe, drying coffee cherries are turned on raised beds for 48 hours to ensure an incredibly consistent, high-quality product.',
         brewType: 'Drip',
-        grind: 'Medium',
-        bloom: '35 Seconds',
-        heat: '210F',
+        grind: 'Medium-fine',
+        bloom: '0 Seconds',
+        heat: '200F',
         id: 1,
-        buyUrl: 'https://www.dancinggoats.com/collections/single-origin/products/ethiopia-sidamo-guji'
+        buyUrl: 'https://manyworldstavern.com/products/treasured-realm'
   };
--- brew-and-tunes/src/components/header.jsx
+++ brew-and-tunes/src/components/header.jsx
@@ -4,15 +4,12 @@ import logo from '../images/brew-and-tunes-logo.png'
 class Header extends React.Component {
     render() {
       return (
-        <header className="columns ">
+        <header className="columns " style={{alignItems: "center"}}>
             <div className="column level-item has-text-centered is-one-quarter-tablet is-full-mobile">
             <img src={logo} className="logo" alt="logo" />
             </div>
-            <div className="column">
-              <h1>Brew and Tunes</h1>
-              <p>Brew and Tunes is a side project to share my weekly playlist and coffee selection. Each week I rotate through albums that I have purchased, mostly through Bandcamp. I listen to albums front to back because listening to the same single over and over drives me insane; so this format of listing albums fits my normal routine. Coffee is a constant that is ever changing, for that last year I have been on an African kick, but that does change from time to time as I find new beans that pique my curiosity. 
-              </p>
-              <p>As I develop this project more I might expand information to include the hardware I am using for my coffee and my music listening.</p>
+            <div className="column" >
+              <h1>Brew & Tunes</h1>
           </div>
         </header>
       );
