18334f443f0fc528e773eb1b39872334e716dc2d
Ian-CM <30392014+broopa@users.noreply.github.com>
2022-04-20 16:47:27 -0400

    Updates 4/20
    Updated background & Items

--- brew-and-tunes/src/components/data/coffee.js
+++ brew-and-tunes/src/components/data/coffee.js
@@ -1,13 +1,13 @@
 export const WeeklyCoffee = {
-        name: 'Misty Valley',
-        roaster: 'Many Worlds Tavern',
-        origin: 'Yirga Chefe, Ethiopia',
-        imageUrl: 'https://cdn.shopify.com/s/files/1/0521/0921/3887/t/8/assets/treasured-realm--web-1637297829659.png?v=1637297838',
-        desc: 'Naturally-processed Ethiopian coffee is prized for its fruitiness and complexity.  The Misty Valley is a beautiful example from the Yirgacheffe region. With a coffee this fruity and intensely flavored, we take a lighter approach to the roasting profile to let the beans shine on their own. From producer Abdullah Bagersh, the ripe, drying coffee cherries are turned on raised beds for 48 hours to ensure an incredibly consistent, high-quality product.',
+        name: 'Have You Tried Drinking Coffee?',
+        roaster: 'Sommus Coffee',
+        origin: 'Mexico',
+        imageUrl: 'https://f4.bcbits.com/img/0027686289_10.jpg',
+        desc: 'It\'s a medium roast on the lighter side, from Mexico. Undertones include cocoa, almond, and berry.',
         brewType: 'Drip',
         grind: 'Medium-fine',
-        bloom: '0 Seconds',
-        heat: '200F',
+        bloom: '20 Seconds',
+        heat: '205F',
         id: 1,
-        buyUrl: 'https://manyworldstavern.com/products/treasured-realm'
+        buyUrl: 'https://okniceraps.bandcamp.com/album/have-you-tried-being-happy'
   };
